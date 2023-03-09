2616a7635364fbbeebe725694a4d3105fa7dd3ed
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-03-09 12:45:47 +0300

    initial data

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿String[] InputArray = {"2","Milan",":-)"};
+System.Console.WriteLine($"{InputArray} ");
+String[] OutputArray = new String[] {};
+
+int length = InputArray.Length;
+int i = 0;
+int k = 0;
+
+
